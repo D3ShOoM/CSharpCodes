f9225b56d136d06d40d0d928d0059fa9617bf0be
D3ShOoM <D3ShOoM@github.com>
2017-11-21 01:09:25 +0300

    Frist Vairable

---
+++ FristVariable/FristVariable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FristVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12;
+            Console.Write("My Variable value ={0}", x);
+            Console.ReadKey();
+        }
+    }
+}
