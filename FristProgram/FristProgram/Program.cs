7c8de5c74ae75ddb9afed1fabf3b3ddd86a1e6c1
D3ShOoM <D3ShOoM@github.com>
2017-11-21 01:05:45 +0300

    Frist CSharp Program

---
+++ FristProgram/FristProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FristProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is my frist program in C#
+            Console.Write("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
